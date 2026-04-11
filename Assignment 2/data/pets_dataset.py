fix output scaling for bbox

Assignment 2/data/pets_dataset.py
@@ -81,6 +81,7 @@ class OxfordIIITPetDataset(Dataset):
         
         # Always load image
         image = Image.open(os.path.join(self.images_dir, f"{img_id}.jpg")).convert("RGB")
+        w_orig, h_orig = image.size
         image = np.array(image)
 
         data = {"image": image}
@@ -113,10 +114,11 @@ class OxfordIIITPetDataset(Dataset):
                 # Convert [xmin, ymin, xmax, ymax] -> [xc, yc, w, h]
                 xmin, ymin, xmax, ymax = transformed['bboxes'][0]
                 data["bbox"] = torch.tensor([
-                    ((xmin + xmax) / 2),
-                    ((ymin + ymax) / 2),
-                    (xmax - xmin),
-                    (ymax - ymin)
+                    ((xmin + xmax) / 2) * w_orig,
+                    ((ymin + ymax) / 2) * h_orig,
+                    (xmax - xmin) * w_orig,
+                    (ymax - ymin) * h_orig
                 ], dtype=torch.float32)
+                data["orig_size"] = torch.tensor([w_orig, h_orig], dtype=torch.float32)
 
         return data

Assignment 2/models/localization.py
@@ -20,7 +20,7 @@ class VGG11Localizer(nn.Module):
             nn.Sigmoid() # Squish to [0, 1] for relative coordinates
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, image_size: torch.Tensor = None) -> torch.Tensor:
         x = self.encoder(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
@@ -28,5 +28,8 @@ class VGG11Localizer(nn.Module):
         # Get normalized coordinates [0, 1]
         coords = self.regression_head(x)
         
-        # Scale to image pixel space (224 x 224)
-        return coords * 224.0
+        if image_size is None:
+            return coords * 224
+
+        scale = torch.stack([image_size[:, 0], image_size[:, 1], image_size[:, 0], image_size[:, 1]], dim=1)
+        return coords * scale

Assignment 2/train.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+import wandb
 
 from data.pets_dataset import OxfordIIITPetDataset
 from models.classification import VGG11Classifier
@@ -68,6 +69,10 @@ def train():
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
+    # WandB Initialization
+    wandb.login(key="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")
+    wandb.init(project="DA6401_Assignment_2", config=args)
+
     # 4. Training Loop
     for epoch in range(args.epochs):
         epoch_start = time.perf_counter()
@@ -81,9 +86,10 @@ def train():
             # For localization, ensure targets are float
             if args.task == 'localization':
                 targets = targets.float()
+                sizes = batch['orig_size'].to(device, non_blocking=pin_memory)
 
             optimizer.zero_grad()
-            outputs = model(images)
+            outputs = model(images, sizes) if args.task == 'localization' else model(images)
             
             loss = criterion(outputs, targets)
             loss.backward()